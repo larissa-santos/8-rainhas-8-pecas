fafce258326dd474dbf9cd2b718abda82887d467
Larissa Santos <larrissapcs@hotmail.com>
2017-06-15 14:28:10 -0300

    Jogo das oito rainhas sem busca por solucao

--- index.php
+++ index.php
@@ -16,100 +16,100 @@
 
 <table class="tabuleiro">
 	<tr>
-		<td class="posicoes" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
-			<div class="queen" draggable="true" ondragstart="drag(event)" id="drag1"></div>
+		<td ataca="" class="posicoes" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
+			<div class="queen" draggable="true" anterior="div1" ondragstart="drag(event)" id="drag1"></div>
 		</td>
-		<td class="posicoes cell-preta" id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div3" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div4" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div5" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div6" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div7" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div8" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div3" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div4" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div5" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div6" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div7" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div8" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 	<tr>
-		<td class="posicoes cell-preta" id="div9" ondrop="drop(event)" ondragover="allowDrop(event)">
-	  		<div class="queen" draggable="true" ondragstart="drag(event)" id="drag2"></div>
+		<td ataca="" class="posicoes cell-preta" id="div9" ondrop="drop(event)" ondragover="allowDrop(event)">
+	  		<div class="queen" draggable="true" anterior="div9" ondragstart="drag(event)" id="drag2"></div>
 		</td>
-		<td class="posicoes" id="div10" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div11" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div12" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div13" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div14" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div15" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div16" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div10" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div11" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div12" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div13" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div14" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div15" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div16" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 	<tr>
-		<td class="posicoes " id="div17" ondrop="drop(event)" ondragover="allowDrop(event)">
-		  <div class="queen" draggable="true" ondragstart="drag(event)" id="drag3"></div>
+		<td ataca="" class="posicoes " id="div17" ondrop="drop(event)" ondragover="allowDrop(event)">
+		  <div class="queen" draggable="true" anterior="div17" ondragstart="drag(event)" id="drag3"></div>
 		</td>
-		<td class="posicoes cell-preta" id="div18" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div19" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div20" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div21" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div22" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div23" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div24" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div18" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div19" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div20" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div21" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div22" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div23" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div24" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 	<tr>
-		<td class="posicoes cell-preta" id="div25" ondrop="drop(event)" ondragover="allowDrop(event)">
-		  <div class="queen" draggable="true" ondragstart="drag(event)" id="drag4"></div>
+		<td ataca="" class="posicoes cell-preta" id="div25" ondrop="drop(event)" ondragover="allowDrop(event)">
+		  <div class="queen" draggable="true" anterior="div25" ondragstart="drag(event)" id="drag4"></div>
 		</td>
-		<td class="posicoes" id="div26" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div27" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div28" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div29" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div30" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div31" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div32" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div26" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div27" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div28" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div29" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div30" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div31" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div32" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 	<tr>
-		<td class="posicoes" id="div33" ondrop="drop(event)" ondragover="allowDrop(event)">
-		  <div class="queen" draggable="true" ondragstart="drag(event)" id="drag5"></div>
+		<td ataca="" class="posicoes" id="div33" ondrop="drop(event)" ondragover="allowDrop(event)">
+		  <div class="queen" draggable="true" anterior="div33" ondragstart="drag(event)" id="drag5"></div>
 		</td>
-		<td class="posicoes cell-preta" id="div34" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div35" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div36" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div37" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div38" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div39" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div40" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div34" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div35" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div36" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div37" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div38" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div39" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div40" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 	<tr>
-		<td class="posicoes cell-preta" id="div41" ondrop="drop(event)" ondragover="allowDrop(event)">
-		  <div class="queen" draggable="true" ondragstart="drag(event)" id="drag6"></div>
+		<td ataca="" class="posicoes cell-preta" id="div41" ondrop="drop(event)" ondragover="allowDrop(event)">
+		  <div class="queen" draggable="true" anterior="div41" ondragstart="drag(event)" id="drag6"></div>
 		</td>
-		<td class="posicoes" id="div42" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div43" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div44" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div45" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div46" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div47" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div48" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div42" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div43" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div44" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div45" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div46" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div47" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div48" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 	<tr>
-		<td class="posicoes" id="div49" ondrop="drop(event)" ondragover="allowDrop(event)">
-		  <div class="queen" draggable="true" ondragstart="drag(event)" id="drag7"></div>
+		<td ataca="" class="posicoes" id="div49" ondrop="drop(event)" ondragover="allowDrop(event)">
+		  <div class="queen" draggable="true" anterior="div49" ondragstart="drag(event)" id="drag7"></div>
 		</td>
-		<td class="posicoes cell-preta" id="div50" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div51" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div52" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div53" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div54" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div55" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div56" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div50" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div51" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div52" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div53" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div54" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div55" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div56" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 	<tr>
-		<td class="posicoes cell-preta" id="div57" ondrop="drop(event)" ondragover="allowDrop(event)">
-		  <div class="queen" draggable="true" ondragstart="drag(event)" id="drag8"></div>
+		<td ataca="" class="posicoes cell-preta" id="div57" ondrop="drop(event)" ondragover="allowDrop(event)">
+		  <div class="queen" draggable="true" anterior="div57" ondragstart="drag(event)" id="drag8"></div>
 		</td>
-		<td class="posicoes" id="div58" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div59" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div60" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div61" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div62" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes cell-preta" id="div63" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
-		<td class="posicoes" id="div64" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div58" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div59" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div60" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div61" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div62" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes cell-preta" id="div63" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
+		<td ataca="" class="posicoes" id="div64" ondrop="drop(event)" ondragover="allowDrop(event)"></td>
 	</tr>
 </table>
 
--- js/8-rainhas.js
+++ js/8-rainhas.js
@@ -1,10 +1,50 @@
-function allowDrop(ev) {
+	var cellAtual = null;
+
+	function allowDrop(ev) {
 	    ev.preventDefault();
 	}
 
 	function drag(ev) {
-		console.log(ev);
 	    ev.dataTransfer.setData("text", ev.target.id);
+	}
+
+	function removeAtaca(remove, local){
+		var atacadas = $("#div"+local).attr('ataca');
+		var att = atacadas.split(",");
+		atacadas = [];
+		for (var i = att.length - 1; i >= 0; i--) {
+			if (att[i] != remove) {
+				atacadas.push(att[i]);
+			}
+		}
+		atacadas = atacadas.join(',');
+
+		// testa se tem algum numero na variavel
+		if (!(/([0-9])+/.test(atacadas))) {
+			// remove alerta de erro e atualiza ataca para vazio
+		 	$("#div"+local).removeClass("cell-error");
+		 	$("#div"+local).attr('ataca', '');
+		} else {
+			$("#div"+local).attr('ataca', atacadas);
+		}
+	}
+
+	function atualizaAtacava(data){
+		var cellAnteiror = $("#"+data).attr("anterior");
+	    var atacava = $("#"+cellAnteiror).attr("ataca");
+
+	    var indice = cellAnteiror.replace("div","");
+
+	    atacava = atacava.split(",");
+	    for (var i = atacava.length - 1; i >= 0; i--) {
+	    	if (parseInt(atacava[i]) > 0) {
+	    		removeAtaca(indice, atacava[i]);
+	    	}
+	    }
+
+	    // remove celulas que atacava e alerta de erro
+	    $("#"+cellAnteiror).attr("ataca",'');
+	    $("#"+cellAnteiror).removeClass("cell-error");
 	}
 
 	function drop(ev) {
@@ -13,65 +53,146 @@ function allowDrop(ev) {
 	    var data = ev.dataTransfer.getData("text");
 	    // transferindo rainha de lugar
 	    ev.target.appendChild(document.getElementById(data));
-	    verificarValidade(ev);
+	    
+	    atualizaAtacava(data);
+	    
+	    var cellAtual = ev.target.id;
+	    $("#"+data).attr("anterior", cellAtual);
+
+	    verificarValidade(ev.target.id);
 	}
 
-	function verificarValidade(ev) {
-		var posicao = parseInt(ev.target.id.replace("div",""));
-		console.log(posicao);
-		var indice = posicao/8;
-		
+	function addAtaca(add, cell){
+		var atacadas = $("#"+cell).attr('ataca');
+		atacadas +=  ',' + add ;	
+				
+		$("#"+cell).attr('ataca', atacadas);
+	}
+
+	function verificaFilho(i, pai) {
+		if ($("#div"+i).children(".queen").length > 0) {
+			$("#div"+i).addClass("cell-error");
+			addAtaca(i, pai);
+			addAtaca(pai.replace("div",""), "div"+i);
+			return 1;
+		} 
+		return 0;
+	}
+
+	function verificarValidade(id) {
+		var posicao = parseInt(id.replace("div",""));
+		var indice = posicao%8;
+		var valida = 0;		
+
 		// horizontal para frente
-		// var i = posicao + 1;
-		// console.log(i + '--');
-		// console.log((i%8));
-		for (var i = posicao + 1; (i%8) != 0; i++) {
-			//if ($("#div"+i).children(".queen"))
-			$("#div"+i).has(".queen").addClass("cell-error");
+		for (var i = posicao + 1; (i%8) > 1; i++) {
+			valida += verificaFilho(i, id);
 		}
-		$("#div"+i).has(".queen").addClass("cell-error");
+		if (indice !== 0) {
+			valida += verificaFilho(i, id);
+		}
 		
 		// horizontal para trás
 		for (var i = posicao - 1; (i%8) > 1; i--) {
-			if ($("#div"+i).children(".queen").lenght > 0) {
-				$("#div"+i).addClass("cell-error");
-			} else {
-				
+			valida += verificaFilho(i, id);
+		}
+		if (indice !== 1)  {
+			valida += verificaFilho(i, id);
+		}
+		
+		// vertical para baixo
+		var i = posicao + 8; 
+		while (i < 65) {
+			valida += verificaFilho(i, id);
+			i += 8;
+		}
+		// vertical para cima
+		var i = posicao - 8; 
+		while (i > 0) {
+			valida += verificaFilho(i, id);
+			i -= 8;
+		}
+
+		// diagonal principal para baixo
+		if ((posicao%8) !== 0) {
+			var i = posicao + 9; 
+			while (i < 65 && (i%8) > 1) {
+				valida += verificaFilho(i, id);
+				i += 9;
+			}
+			if ((i%8) == 0) {
+				valida += verificaFilho(i, id);
 			}
 		}
-		$("#div"+i).has(".queen").addClass("cell-error");
 
-		// diagonal para frente
-		// for (var i = posicao + 9; (i/8) < 8; i+9) {
-		// 	console.log($("#div"+i));
-		// }
-		// $(".posicoes").each(function(){
-		// 	var pos = this.id.replace("div","");
-		// 	var ind = pos/8;
-		// 	console.log(pos);
-		// 	//if (this ){}
+		// diagonal principal para cima
+		var i = posicao - 9; 
+		if ((posicao%8) !== 1) {
+			while (i > 0 && (i%8) > 1) {
+				valida += verificaFilho(i, id);
+				i -= 9;
+			}
+			if ((i%8) == 1) {
+				valida += verificaFilho(i, id);
+			}
+		}
 
-		// });
+		// diagonal secundaria para baixo
+		if ((posicao%8) !== 1) {
+			var i = posicao + 7; 
+			while (i < 64 && (i%8) > 1) {
+				valida += verificaFilho(i, id);
+				i += 7;
+			}
+			if (i < 64 && (i%8) == 1) {
+				valida += verificaFilho(i, id);
+			}
+		}
+		
+		// diagonal secundaria para cima
+		var i = posicao - 7; 
+		if ((posicao%8) !== 0) {
+			while (i > 1 && (i%8) > 1) {
+				valida += verificaFilho(i, id);
+				i -= 7;
+			}
+			if (i > 1 && (i%8) == 0) {
+				valida += verificaFilho(i, id);
+			}
+		}
+
+		if (valida > 0)	$("#"+id).addClass("cell-error");
 	}
 
 	function solucao() {
-		$('#div2').append($("#drag1"));
-		$('#div12').append($("#drag2"));
-		$('#div22').append($("#drag3"));
-		$('#div32').append($("#drag4"));
-		$('#div35').append($("#drag5"));
-		$('#div41').append($("#drag6"));
-		$('#div55').append($("#drag7"));
-		$('#div61').append($("#drag8"));
+		var arrayDiv = ['div2','div12','div22','div32','div35','div41','div55','div61'];
+		var arrayDrag = ["#drag1","#drag2","#drag3","#drag4","#drag5","#drag6","#drag7","#drag8"];
+		
+		$(".posicoes").each(function(){
+			$(this).removeClass("cell-error");
+			$(this).attr("ataca","");
+		});
+
+		for (var i = arrayDiv.length - 1; i >= 0; i--) {
+			$("#" + arrayDiv[i]).append($(arrayDrag[i]));
+			$(arrayDrag[i]).attr("anterior", arrayDiv[i]);
+		}
+		for (var i = arrayDiv.length - 1; i >= 0; i--) verificarValidade(arrayDiv[i]);
 	}
 
 	function recomecar() {
-		$('#div1').append($("#drag1"));
-		$('#div9').append($("#drag2"));
-		$('#div17').append($("#drag3"));
-		$('#div25').append($("#drag4"));
-		$('#div33').append($("#drag5"));
-		$('#div41').append($("#drag6"));
-		$('#div49').append($("#drag7"));
-		$('#div57').append($("#drag8"));
+		var arrayDiv = ['div1','div9','div17','div25','div33','div41','div49','div57'];
+		var arrayDrag = ["#drag1","#drag2","#drag3","#drag4","#drag5","#drag6","#drag7","#drag8"];
+		
+		$(".posicoes").each(function(){
+			$(this).removeClass("cell-error");
+			$(this).attr("ataca","");
+		});
+
+		for (var i = arrayDiv.length - 1; i >= 0; i--) {
+			$("#" + arrayDiv[i]).append($(arrayDrag[i]));
+			$(arrayDrag[i]).attr("anterior", arrayDiv[i]);
+		}
+
+		for (var i = arrayDiv.length - 1; i >= 0; i--) verificarValidade(arrayDiv[i]);
 	}
